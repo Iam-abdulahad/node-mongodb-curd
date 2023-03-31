437eb8ee73881c2698c732e6ade918217eb8cefc
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-03-31 09:14:54 +0600

    delete user from database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const objectId = require('mongodb').objectId;
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,19 +22,31 @@ async function run() {
     await client.connect();
     const userCollection = client.db("foodExpress").collection("user");
 
-    app.get('/user', async (req, res)=>{
+    // get users
+    app.get('/user', async (req, res) => {
       const query = {};
       const curser = userCollection.find(query);
       const user = await curser.toArray();
       res.send(user);
     })
 
-    app.post('/user', async(req, res)=>{
+
+    // POST User : add a new user
+    app.post('/user', async (req, res) => {
       const newUser = req.body;
       console.log('new user added ', newUser);
       const result = await userCollection.insertOne(newUser);
       res.send(result)
     })
+
+     // delete a user
+
+     app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: objectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+     })
 
   }
   finally {
